fb31ad6a2ee8a40722a3291a0e837525a489fe6b
Dilshoda Abdualimova <dilshoda@Dilshodas-MacBook-Pro.local>
2023-06-06 20:53:41 -0700

    core functionality && design done

---
+++ migrations/20230607020405-create-votes.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('votes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      pollId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'polls',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      optionId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'pollOptions',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('votes');
+  }
+};
