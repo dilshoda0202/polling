858e1d1911255c9748c40239a898747643b13ee5
Dilshoda Abdualimova <dilshoda@Dilshodas-MacBook-Pro.local>
2023-06-06 20:53:41 -0700

    chore: clean up server.js

--- server.js
+++ server.js
@@ -9,20 +9,6 @@ const isLoggedIn = require('./middleware/isLoggedIn');
 const axios = require('axios');
 const models = require('./models')
 const DbStore = require('./dbstore');
-// home route
-app.get('/movies', function (req, res) {
-  const url = 'https://serpapi.com/search.json?q=eternals+theater&location=Austin,+Texas,+United+States&hl=en&gl=us'
-  axios.get(url, {params: {api_key: process.env.API_KEY}})
-    .then(function (response) {
-      console.log('test data', response.data)
-      // handle success
-      return res.render('movies', {movies: response.data});
-    })
-    .catch(function (error) {
-      res.json({message: 'Data not found. Please try again later.'});
-    });
-});
-
 
 SECRET_SESSION = process.env.SECRET_SESSION;
 // console.log('>>>>', SECRET_SESSION);
@@ -30,7 +16,7 @@ SECRET_SESSION = process.env.SECRET_SESSION;
 app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(layouts);
@@ -46,7 +32,6 @@ app.use(session({
 app.use(passport.initialize());      // Initialize passport
 app.use(passport.session());         // Add a session
 
-
 app.use((req, res, next) => {
   res.locals.alerts = req.flash();
   res.locals.currentUser = req.user;
@@ -55,14 +40,28 @@ app.use((req, res, next) => {
 
 app.get('/', isLoggedIn, async (req, res) => {
   const query = await models.poll.findAll({
-    include: [{model: models.pollOption, as: 'options'}, {model: models.votes, as: 'votes'}],
+    include: [{ model: models.pollOption, as: 'options' }, { model: models.votes, as: 'votes' }],
   });
-  const polls = query.map(el => el.get({plain: true}));
+  const polls = query.map(el => el.get({ plain: true }));
   polls.forEach(p => {
     p.hasVoted = !!p.votes.find(v => v.userId === req.user.id);
   });
   console.log(polls[0]);
-  res.render('index', {polls});
+  res.render('index', { polls });
+});
+
+// home route
+app.get('/movies', function (req, res) {
+  const url = 'https://serpapi.com/search.json?q=eternals+theater&location=Austin,+Texas,+United+States&hl=en&gl=us'
+  axios.get(url, { params: { api_key: process.env.API_KEY } })
+    .then(function (response) {
+      console.log('test data', response.data)
+      // handle success
+      return res.render('movies', { movies: response.data });
+    })
+    .catch(function (error) {
+      res.json({ message: 'Data not found. Please try again later.' });
+    });
 });
 
 app.get('/newpoll', isLoggedIn, (req, res) => {
@@ -74,8 +73,8 @@ app.use('/polls', require('./controllers/polls'));
 
 // Add this above /auth controllers
 app.get('/profile', isLoggedIn, (req, res) => {
-  const {id, name, email} = req.user.get();
-  res.render('profile', {id, name, email});
+  const { id, name, email } = req.user.get();
+  res.render('profile', { id, name, email });
 });
 
 const PORT = process.env.PORT || 3000;
